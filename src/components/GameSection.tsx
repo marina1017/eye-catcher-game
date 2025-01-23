f266e74bbdd63eec8a08e4379fee4f760d168124
marina1017 <mari10172001@yahoo.co.jp>
2025-01-24 01:59:15 +0900

    矢印の追加

--- src/components/GameSection.tsx
+++ src/components/GameSection.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import '../styles/GameSection.scss';
+import { ArrowBigDownDashIcon } from 'lucide-react';
 
 const GameSection = () => {
   return (
     <div className="game-section__container">
       <section className="game-section__section">
-        <h2>イントロ</h2>
+        <h2 className='game-section__title'>広告イライラゲーム</h2>
+        <p className='game-section_description'>
+          このゲームはカメラを使用します。準備ができたら下にスクロールしてください
+        </p>
+        <ArrowBigDownDashIcon className='game-section_scroll-indicator' size={60} strokeWidth={2}></ArrowBigDownDashIcon>
       </section>
       
       <section className="game-section__section">
-        <h2>キャリブレーション</h2>
+        <h2 className='game-section__title'>キャリブレーション</h2>
       </section>
       
       <section className="game-section__section">
-        <h2>ゲーム</h2>
+        <h2 className='game-section__title'>ゲーム</h2>
       </section>
     </div>
   );
