800f0acb75dc56062ce71f8f523727a17b1a8867
marina1017 <mari10172001@yahoo.co.jp>
2025-01-24 00:46:14 +0900

    GameSectionの追加

---
+++ src/components/GameSection.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const GameSection = () =>{
+  return (
+    <div>
+      <section>イントロ</section>
+      <section>キャリブレーション</section>
+      <section>ゲーム画面</section>
+    </div>
+  )
+}
+
+export default GameSection;
