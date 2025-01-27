e016b5c191c63fb0bafbfd912d036f62983de56b
null <null>
2025-01-27 23:14:52 +0900

    カメラ画像をスクロールイベントに合わせて表示する

--- src/components/GameSection.tsx
+++ src/components/GameSection.tsx
@@ -3,28 +3,49 @@ import "../styles/GameSection.scss";
 import { ArrowBigDownDashIcon, Camera } from "lucide-react";
 
 const GameSection = () => {
+  const [currentSection, setCurrentSection] = useState(0);
   const [stream, setStream] = useState<MediaStream | null>(null);
 
+  // カメラの画像をコントロール
   useEffect(() => {
-    const startCamera = async () => {
-      try {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        setStream(mediaStream);
-      } catch (err) {
-        console.error("カメラの起動に失敗しました");
-      }
+    if (currentSection === 1) {
+      const startCamera = async () => {
+        try {
+          const mediaStream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          setStream(mediaStream);
+        } catch (err) {
+          console.error("カメラの起動に失敗しました");
+        }
+      };
+
+      startCamera();
+
+      // クリーンアップ関数(コンポーネントが消えるときに実行)
+      return () => {
+        // streamに入っている各トラックを停止させる
+        stream?.getTracks().forEach((track) => track.stop());
+        // streamの状態をクリア
+        setStream(null);
+      };
+    }
+  }, [currentSection]);
+
+  // スクロールをコントロール
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollHeight = window.innerHeight;
+      const section = Math.floor(scrollPosition / scrollHeight);
+      setCurrentSection(section);
     };
 
-    startCamera();
+    window.addEventListener("scroll", handleScroll);
 
     // クリーンアップ関数(コンポーネントが消えるときに実行)
     return () => {
-      // streamに入っている各トラックを停止させる
-      stream?.getTracks().forEach((track) => track.stop());
-      // streamの状態をクリア
-      setStream(null);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -45,17 +66,20 @@ const GameSection = () => {
       <section className="game-section__section">
         <h2 className="game-section__title">キャリブレーション</h2>
         <div className="game-section__camera-feed">
-          <Camera size={48} />
-          <div>
-            <video
-              autoPlay
-              ref={(videoRef) => {
-                if (videoRef && stream) {
-                  videoRef.srcObject = stream;
-                }
-              }}
-            />
-          </div>
+          {!stream ? (
+            <Camera size={48} />
+          ) : (
+            <div>
+              <video
+                autoPlay
+                ref={(videoRef) => {
+                  if (videoRef && stream) {
+                    videoRef.srcObject = stream;
+                  }
+                }}
+              />
+            </div>
+          )}
         </div>
       </section>
 
